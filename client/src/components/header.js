4cd5cb3cfd0b64cf482030e740a17afd288dd0c5
DrishtiAga05 <aga.drishti05@gmail.com>
2026-05-26 12:54:21 +0530

    additional proxy rules

---
+++ client/src/components/header.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return (
+            <nav>
+                <div className="nav-wrapper">
+                    <a href="/" className="left brand-logo">
+                        Feedback-collector
+                    </a>
+
+                    <ul className="right">
+                        <li>
+                            <a href="/auth/google">
+                                Login With Google
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Header;
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,22 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
 
 const el = document.getElementById('root');
-
 const root = ReactDOM.createRoot(el);
 
-const store = createStore(reducers);
+const store = createStore(
+    reducers,
+    {},
+    applyMiddleware(reduxThunk)
+);
 
 root.render(
     <Provider store={store}>
