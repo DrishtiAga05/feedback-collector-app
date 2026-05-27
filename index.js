1c3766c032778137fb8ec1aa3897df619322ba0a
DrishtiAga05 <aga.drishti05@gmail.com>
2026-05-27 16:55:59 +0530

    fix express version crash

--- index.js
+++ index.js
@@ -35,13 +35,13 @@ app.use(passport.session());
 
 // ===== PRODUCTION STATIC FILES =====
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(
-            path.resolve(__dirname, 'client', 'build', 'index.html')
-        );
-    });
+    res.sendFile(
+        path.resolve(__dirname, 'client', 'build', 'index.html')
+    );
+});
 }
 
 // Start server
