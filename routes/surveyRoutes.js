17138cbcdbedcf674dcef0fabb9607588cdb162a
DrishtiAga05 <aga.drishti05@gmail.com>
2026-06-02 15:33:06 +0530

    redirect to dashboard

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -11,28 +11,33 @@ module.exports = app => {
         res.send('Thanks for voting!');
     });
 
-    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
-        const { title, subject, body, recipients } = req.body;
+    app.post(
+        '/api/surveys', 
+        requireLogin, 
+        //requireCredits, 
+        async (req, res) => {
+            const { title, subject, body, recipients } = req.body;
 
-        const survey= new Survey({
-            title,
-            subject,
-            body,
-            recipients: recipients.split(',').map(email => ({ email: email.trim() })),
-            _user: req.user.id,
-            dateSent: Date.now()
-        });
-        const mailer= new Mailer(survey, surveyTemplate(survey));
+            const survey= new Survey({
+                title,
+                subject,
+                body,
+                recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+                _user: req.user.id,
+                dateSent: Date.now()
+            });
+            const mailer= new Mailer(survey, surveyTemplate(survey));
 
-        try{
-            await mailer.send();
-            await survey.save();
-            req.user.credits-= 1;
-            const user = await req.user.save();
+            try{
+                await mailer.send();
+                await survey.save();
+                //req.user.credits-= 1;
+                const user = await req.user.save();
 
-            res.send(user);
-        } catch (err) {
-            res.status(422).send(err);
+                res.send(user);
+            } catch (err) {
+                res.status(422).send(err);
+            }
         }
-    });
+    );
 };
